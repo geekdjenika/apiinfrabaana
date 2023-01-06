4a7f3464d78bbacce3c3b29434a91620b2a6d225
Geek DJENIKA <djenikaa@gmail.com>
2023-01-06 22:40:29 +0000

    Importation du projet

---
+++ src/main/java/ml/geekdjenika/apiinfrabaana/ApiinfrabaanaApplication.java
@@ -0,0 +1,13 @@
+package ml.geekdjenika.apiinfrabaana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiinfrabaanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiinfrabaanaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ml/geekdjenika/apiinfrabaana/ApiinfrabaanaApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.geekdjenika.apiinfrabaana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiinfrabaanaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
